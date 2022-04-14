b5a1f8a60855023f1ead0a07ff7e203a3ac34151
Tolulope Arinola <specialgrace.at@gmail.com>
2022-04-14 16:55:23 -0700

    mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *mul - multiples two ints.
+ *@a: integer.
+ *@b: integer.
+ *
+ * Return: product.
+ */
+int mul(int a, int b)
+{
+	int product = a * b;
+
+	return (product);
+}
