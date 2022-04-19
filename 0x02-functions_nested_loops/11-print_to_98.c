35b86be48b9884ca305fb9baa5cfdf943eca5cf0
Tolulope Arinola <specialgrace.at@gmail.com>
2022-04-19 06:01:13 -0700

    11 correction

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,18 +10,16 @@ void print_to_98(int n)
 {
 	if (n < 98)
 	{
-		while (n < 98)
+		for (; n < 98; n++)
 		{
 			printf("%d, ", n);
-			n++
 		}
 	}
 	else if (n > 98)
 	{
-		while (n > 98)
+		for (; n > 98; n--)
 		{
 			printf("%d, ", n);
-			n--;
 		}
 	}
 	printf("%d\n", n);
