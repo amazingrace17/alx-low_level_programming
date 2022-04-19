7c47651db9236d1d5e6ec070b785c57559222b62
Tolulope Arinola <specialgrace.at@gmail.com>
2022-04-19 04:51:35 -0700

    7-10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add -adds integers
+ * @n1: n1 is an integer
+ * @n2: n2 is an integer
+ *
+ * Return: sum
+ */
+int add(int n1, int n2)
+{
+	int sum;
+
+	sum = n1 + n2;
+
+	return (sum);
+}
